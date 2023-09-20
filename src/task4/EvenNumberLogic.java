7b004c25eac5e34aaa321eb2dd8e170b776d61a6
GlebGus <gusinec.lion.com@tut.by>
2023-09-20 18:44:54 +0300

    bug-fix EvenNumberLogic

--- src/task4/EvenNumberLogic.java
+++ src/task4/EvenNumberLogic.java
@@ -2,40 +2,40 @@ package task4;
 
 public class EvenNumberLogic {
     public boolean evenNumber(int a, int b, int c, int d) {
-        if (a % 2 == 0 && Math.abs(a) >= 10) {
-            if (b % 2 == 0 && Math.abs(b) >= 10) {
+        if (Math.abs(a) % 2 == 0) {
+            if (Math.abs(b) % 2 == 0  ) {
                 return true;
             }
         }
-        if (a % 2 == 0 && Math.abs(a) >= 10) {
-            if (c % 2 == 0 && Math.abs(c) >= 10) {
+        if (Math.abs(a) % 2 == 0 ) {
+            if (Math.abs(c) % 2 == 0 ) {
                 return true;
             }
         }
-        if (b % 2 == 0 && Math.abs(b) >= 10) {
-            if (c % 2 == 0 && Math.abs(c) >= 10) {
+        if (Math.abs(b) % 2 == 0 ) {
+            if (Math.abs(c) % 2 == 0 ) {
                 return true;
             }
         }
-        if (c % 2 == 0 && Math.abs(c) >= 10) {
-            if (d % 2 == 0 && Math.abs(d) >= 10) {
+        if (Math.abs(c) % 2 == 0 ) {
+            if (Math.abs(d) % 2 == 0) {
                 return true;
             }
         }
-        if (b % 2 == 0 && Math.abs(b) >= 10) {
-            if (d % 2 == 0 && Math.abs(d) >= 10) {
+        if (Math.abs(b) % 2 == 0 ) {
+            if (Math.abs(d) % 2 == 0 ) {
                 return true;
             }
         }
-        if (a % 2 == 0 && Math.abs(a) >= 10) {
-            if (d % 2 == 0 && Math.abs(d) >= 10) {
+        if (Math.abs(a) % 2 == 0) {
+            if (Math.abs(d) % 2 == 0 ) {
                 return true;
             }
         }
-        if (a % 2 == 0 && Math.abs(a) >= 10) {
-            if (d % 2 == 0 && Math.abs(d) >= 10) {
-                if (b % 2 == 0 && Math.abs(b) >= 10) {
-                    if (c % 2 == 0 && Math.abs(c) >= 10) {
+        if (Math.abs(a) % 2 == 0 ) {
+            if (Math.abs(d) % 2 == 0 ) {
+                if (Math.abs(b) % 2 == 0) {
+                    if (Math.abs(c) % 2 == 0 ) {
                         return true;
                     }
                 }
