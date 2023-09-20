8b760b59a9b972f2576eeafd83284ce256718030
GlebGus <gusinec.lion.com@tut.by>
2023-09-20 17:56:58 +0300

    bug-fix

--- src/task4/EvenNumberLogic.java
+++ src/task4/EvenNumberLogic.java
@@ -2,18 +2,23 @@ package task4;
 
 public class EvenNumberLogic {
     public boolean evenNumber(int a, int b, int c, int d) {
-        if (a % 2 == 0 && a >= 10) {
-            if (b % 2 == 0 && b >= 10) {
+        if (a % 2 == 0 && Math.abs(a) >= 10) {
+            if (b % 2 == 0 && Math.abs(b) >= 10) {
                 return true;
             }
         }
-        if (a % 2 == 0 && a >= 10) {
-            if (c % 2 == 0 && c >= 10) {
+        if (a % 2 == 0 && Math.abs(a) >= 10) {
+            if (c % 2 == 0 && Math.abs(c) >= 10) {
                 return true;
             }
         }
-        if (a % 2 == 0 && a >= 10) {
-            if (d % 2 == 0 && d >= 10) {
+        if (b % 2 == 0 && Math.abs(b) >= 10) {
+            if (c % 2 == 0 && Math.abs(c) >= 10) {
+                return true;
+            }
+        }
+        if (a % 2 == 0 && Math.abs(a) >= 10) {
+            if (d % 2 == 0 && Math.abs(d) >= 10) {
                 return true;
             }
         }
