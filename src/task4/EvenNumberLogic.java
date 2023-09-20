159990c20b7f204e1ad1b8e79aedf5165853d2c6
GlebGus <gusinec.lion.com@tut.by>
2023-09-20 18:07:46 +0300

    bug-fix EvenNumberLogic

--- src/task4/EvenNumberLogic.java
+++ src/task4/EvenNumberLogic.java
@@ -17,9 +17,28 @@ public class EvenNumberLogic {
                 return true;
             }
         }
+        if (c % 2 == 0 && Math.abs(c) >= 10) {
+            if (d % 2 == 0 && Math.abs(d) >= 10) {
+                return true;
+            }
+        }
+        if (b % 2 == 0 && Math.abs(b) >= 10) {
+            if (d % 2 == 0 && Math.abs(d) >= 10) {
+                return true;
+            }
+        }
         if (a % 2 == 0 && Math.abs(a) >= 10) {
             if (d % 2 == 0 && Math.abs(d) >= 10) {
                 return true;
+            }
+        }
+        if (a % 2 == 0 && Math.abs(a) >= 10) {
+            if (d % 2 == 0 && Math.abs(d) >= 10) {
+                if (b % 2 == 0 && Math.abs(b) >= 10) {
+                    if (c % 2 == 0 && Math.abs(c) >= 10) {
+                        return true;
+                    }
+                }
             }
         }
         return false;
