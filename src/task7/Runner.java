0349c7f5191cf6af6d6f5ce2a1f1a943d3fbb127
GlebGus <gusinec.lion.com@tut.by>
2023-09-22 18:06:49 +0300

    complete task 7

---
+++ src/task7/Runner.java
@@ -0,0 +1,18 @@
+package task7;
+
+import scanner.DataScanner;
+
+public class Runner {
+    public static void main(String[] args) {
+        ClosestPointLogic closestPointLogic = new ClosestPointLogic();
+        System.out.println("Введите x1: ");
+        int x1 = DataScanner.enterIntegerFromConsole();
+        System.out.println("Введите y1: ");
+        int y1 = DataScanner.enterIntegerFromConsole();
+        System.out.println("Введите x2: ");
+        int x2 = DataScanner.enterIntegerFromConsole();
+        System.out.println("Введите y2: ");
+        int y2 = DataScanner.enterIntegerFromConsole();
+        System.out.println(closestPointLogic.closerToOrigin(x1,y1,x2,y2));
+    }
+}
