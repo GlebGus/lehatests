188ae7292e83df64267dc8b5ab00aeca66dffc00
GlebGus <gusinec.lion.com@tut.by>
2023-09-22 22:38:04 +0300

    timeHasPassed fix method

--- src/task6/TimeLogic.java
+++ src/task6/TimeLogic.java
@@ -5,6 +5,9 @@ public class TimeLogic {
         int hours = seconds / 3600;
         int minutes = (seconds % 3600) / 60;
         int remainingSeconds = seconds % 60;
+        if (seconds >= 86401) {
+            throw new IllegalArgumentException();
+        }
         return "Часов: " + hours + ", минут: " + minutes + ", секунд: " + remainingSeconds;
     }
 }
