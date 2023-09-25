d8687be794b41e764cc082c2f030260d83d60135
GlebGus <gusinec.lion.com@tut.by>
2023-09-25 18:56:45 +0300

    change int to double in closerToOrigin and Runner

--- src/task7/ClosestPointLogic.java
+++ src/task7/ClosestPointLogic.java
@@ -1,7 +1,7 @@
 package task7;
 
 public class ClosestPointLogic {
-    public String closerToOrigin(int x1, int y1, int x2, int y2) {
+    public String closerToOrigin(double x1, double y1, double x2, double y2) {
         double distance1 = Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
         double distance2 = Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2));
 
--- src/task7/Runner.java
+++ src/task7/Runner.java
@@ -6,13 +6,13 @@ public class Runner {
     public static void main(String[] args) {
         ClosestPointLogic closestPointLogic = new ClosestPointLogic();
         System.out.println("Введите x1: ");
-        int x1 = DataScanner.enterIntegerFromConsole();
+        double x1 = DataScanner.enterIntegerFromConsole();
         System.out.println("Введите y1: ");
-        int y1 = DataScanner.enterIntegerFromConsole();
+        double y1 = DataScanner.enterIntegerFromConsole();
         System.out.println("Введите x2: ");
-        int x2 = DataScanner.enterIntegerFromConsole();
+        double x2 = DataScanner.enterIntegerFromConsole();
         System.out.println("Введите y2: ");
-        int y2 = DataScanner.enterIntegerFromConsole();
+        double y2 = DataScanner.enterIntegerFromConsole();
         System.out.println(closestPointLogic.closerToOrigin(x1, y1, x2, y2));
     }
 }
