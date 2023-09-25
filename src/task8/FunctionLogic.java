e69aa60ec349fe46249375fce30afd677fcf6970
Gleb <97987220+GlebGus@users.noreply.github.com>
2023-09-25 22:38:05 +0300

    Update FunctionLogic.java
    насрал

--- src/task8/FunctionLogic.java
+++ src/task8/FunctionLogic.java
@@ -1,7 +1,7 @@
 package task8;
 
 public class FunctionLogic {
-    public double function(double x) { // Задайте значение переменной x
+    public double function(double x) {
         double result;
         if (x >= 3) {
             return result = -x * x + 3 * x + 9;
