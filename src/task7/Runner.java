9bfa6a434db5ebe43207f0c5520284eb0557fee2
GlebGus <gusinec.lion.com@tut.by>
2023-09-26 10:47:21 +0300

    change int to double in Runner

--- src/task7/Runner.java
+++ src/task7/Runner.java
@@ -6,13 +6,13 @@ public class Runner {
     public static void main(String[] args) {
         ClosestPointLogic closestPointLogic = new ClosestPointLogic();
         System.out.println("Введите x1: ");
-        double x1 = DataScanner.enterIntegerFromConsole();
+        double x1 = DataScanner.enterDoubleFromConsole();
         System.out.println("Введите y1: ");
-        double y1 = DataScanner.enterIntegerFromConsole();
+        double y1 = DataScanner.enterDoubleFromConsole();
         System.out.println("Введите x2: ");
-        double x2 = DataScanner.enterIntegerFromConsole();
+        double x2 = DataScanner.enterDoubleFromConsole();
         System.out.println("Введите y2: ");
-        double y2 = DataScanner.enterIntegerFromConsole();
+        double y2 = DataScanner.enterDoubleFromConsole();
         System.out.println(closestPointLogic.closerToOrigin(x1, y1, x2, y2));
     }
 }
