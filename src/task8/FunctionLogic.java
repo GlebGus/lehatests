954bfa4e04472a6e2c7a7a906f7c8596e8733305
GlebGus <gusinec.lion.com@tut.by>
2023-09-29 17:41:28 +0300

    resolve function

--- src/task8/FunctionLogic.java
+++ src/task8/FunctionLogic.java
@@ -2,11 +2,16 @@ package task8;
 
 public class FunctionLogic {
     public double function(double x) {
-        double result;
-        if (x >= 3) {
-            return result = -x * x + 3 * x + 9;
-        } else {
-            return result = 1 / ((x * x * x) -6);
+        double result = 0;
+        try {
+            if (x >= 3) {
+                return result = -x * x + 3 * x + 9;
+            } else {
+                return result = 1 / ((x * x * x) - 6);
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("На ноль делить нельзя");
         }
+        return result;
     }
 }
