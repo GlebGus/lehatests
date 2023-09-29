17587a5132b66e1e79687c081bd01c69414a1f75
GlebGus <gusinec.lion.com@tut.by>
2023-09-29 20:11:33 +0300

    resolve PerfectNumberLogic

--- src/task5/PerfectNumberLogic.java
+++ src/task5/PerfectNumberLogic.java
@@ -2,14 +2,12 @@ package task5;
 
 public class PerfectNumberLogic {
     public boolean isPerfectNumber(int number) {
-        int sum = 0;
-
-        for (int i = 1; i < number; i++) {
+        int sum = 1;
+        for (int i = 2; i <= number / 2; i++) {
             if (number % i == 0) {
                 sum += i;
             }
         }
-
         return sum == number;
     }
 }
